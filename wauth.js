4e3f007e5ee7c18d352dcde7471aa74a53d87970
Rolly Maulana Awangga <rolly@awang.ga>
2023-01-23 06:41:34 +0700

    config essential

--- wauth.js
+++ wauth.js
@@ -1,18 +1,9 @@
 // /*config in html login page*/
-const using_click = true;
 const id_user = 'user_name';
 const id_pass = 'user_pass';
 const id_form = 'loginform';
 const id_button = 'login';
-
-const auth_ws = 'd3NzOi8vYXV0aC51bGJpLmFjLmlkL3dzL3doYXRzYXV0aC9xcg==';
-const keyword = 'aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgw';
-
-const interval = 30;
-const maxqrwait = 90;
-
-const id_qr = "whatsauthqr";
-const id_counter = "whatsauthcounter";
+const using_click = true;
 // /*end of config in html login page*/
 
 /*whatsauth/svgqrjs */
@@ -27,6 +18,12 @@ var refreshbutton=`
 <path style="fill:#F7F7F7;" d="M400.813,169.581c-2.502-4.865-14.695-16.012-35.262-5.891 c-20.564,10.122-10.625,32.351-10.625,32.351c7.666,15.722,11.98,33.371,11.98,52.046c0,65.622-53.201,118.824-118.828,118.824 c-65.619,0-118.82-53.202-118.82-118.824c0-61.422,46.6-111.946,106.357-118.173v30.793c0,0-0.084,1.836,1.828,2.999 c1.906,1.163,3.818,0,3.818,0l98.576-58.083c0,0,2.211-1.162,2.211-3.436c0-1.873-2.211-3.205-2.211-3.205l-98.248-57.754 c0,0-2.24-1.605-4.23-0.826c-1.988,0.773-1.744,3.481-1.744,3.481v32.993c-88.998,6.392-159.23,80.563-159.23,171.21 c0,94.824,76.873,171.696,171.693,171.696c94.828,0,171.707-76.872,171.707-171.696 C419.786,219.788,412.933,193.106,400.813,169.581z"></path>
 </g></svg>
 `;
+const auth_ws = 'd3NzOi8vYXV0aC51bGJpLmFjLmlkL3dzL3doYXRzYXV0aC9xcg==';
+const keyword = 'aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgw';
+const interval = 30;
+const maxqrwait = 90;
+const id_qr = "whatsauthqr";
+const id_counter = "whatsauthcounter";
 
 const apphost = btoa(document.location.protocol + '//' +document.location.host+document.location.pathname);
 let jsonres;
